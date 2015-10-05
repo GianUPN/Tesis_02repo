8a21145eb134f0ed80e10366431c86b7f4894a3c
GianUPN <danycalderonp@hotmail.com>
2015-10-04 23:34:39 -0500

    agregacion de core

---
+++ Tesis_02/Tesis_02/Core/DespiertoPantalla.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperMeatBoyPC.Core
+{
+    interface DespiertoPantalla
+    {
+        Boolean despierto { get; set; }
+    }
+}
---
+++ Tesis_02/Tesis_02/Core/Sprite.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using SuperMeatBoyPC.Core;
+
+
+namespace SuperMeatBoyPC.Core
+{
+    public class Sprite
+    {
+        public Animacion animacion { get; protected set; }
+        public float x { get; set; }
+        public float y { get; set; }
+        public float velocidadX { get; set; }
+        public float velocidadY { get; set; }
+        public bool visible { get; set; }
+        public bool movil { get; set; }
+        public bool solidoTiles { get; set; }
+        public bool solidoSprites { get; set; }
+        public float rotacion{get; set; }
+        public Color color;
+        public TileMap escenarioActual { get; set; }
+
+        public Sprite(Animacion animacion) : this(animacion, 0, 0)
+        { 
+        }
+
+        public Sprite(Animacion animacion, float x, float y)
+        {
+            this.animacion = animacion;
+            this.x = x;
+            this.y = y;
+            this.movil = true;
+            this.visible = true;
+            this.solidoSprites = true;
+            this.solidoTiles = true;
+        }
+         
+        virtual public void actualizar(long tiempo)
+        {
+            if (movil)
+            {
+                x = x + velocidadX * tiempo;
+                y = y + velocidadY * tiempo;
+            }
+            animacion.actualizar(tiempo);
+        }
+
+        virtual public void evento_ColisionHorizontalTile()
+        {
+            velocidadX = 0f;
+        }
+
+        virtual public void evento_ColisionVerticalTile()
+        {
+            velocidadY = 0f;
+        }
+
+        virtual public void evento_ColisionHorizontalSprite(Sprite objSprite)
+        {
+        }
+
+        virtual public void evento_ColisionVerticalSprite(Sprite objSprite)
+        {
+        }
+     }
+}
