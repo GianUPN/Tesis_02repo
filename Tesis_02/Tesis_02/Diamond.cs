03add6209ee3ce05257cfb28422069e41aaa8f25
kadoprogram <diefg_16_05@hotmail.com>
2015-10-08 20:52:09 -0500

    jh

---
+++ Tesis_02/Tesis_02/Diamond.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Audio;
+using System.Threading;
+using Tesis_02;
+
+namespace Tesis_02
+{
+    class Diamond
+    {
+
+    }
+}
